7b77a6ee0c101b1ba9a300a6d192859a048eb2b5
Rokas <rookas.rudzenskas@gmail.com>
2022-09-06 09:49:16 +0300

    Rokas

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,15 +1,33 @@
 import React, {useLayoutEffect} from 'react';
-import {Text, View, StyleSheet} from 'react-native';
-
+import {Text, View, StyleSheet, SafeAreaView, Image} from 'react-native';
+import {useNavigation} from "@react-navigation/native";
+import { Ionicons } from '@expo/vector-icons';
 const HomeScreen = () => {
-    useLayoutEffect(() => {
+    const navigation = useNavigation();
 
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerTitle: 'Uizard Wifi Testing',
+            headerShown: false
+        });
     }, []);
 
     return (
-        <View>
-            <Text>Hello, this is working fine!</Text>
-        </View>
+        <SafeAreaView>
+            <View className="flex-row pb-3 items-center mx-4 space-x-2">
+                <Image
+                    source={{uri: 'https://links.papareact.com/wru'}}
+                    className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+                />
+                <View>
+                    <Text className="font-bold color-gray-400 text-xs">Deliver Now!</Text>
+                    <Text className="font-bold text-xl">Current Location
+                        <Ionicons name="chevron-down-sharp" className="pt-1" size={20} color="#00CCBB" />
+                    </Text>
+
+                </View>
+            </View>
+        </SafeAreaView>
     );
 };
 
