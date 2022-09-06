4ffb927060e55cdc288974c16863bdfcaedbb249
Rokas <rookas.rudzenskas@gmail.com>
2022-09-06 11:12:57 +0300

    Rokas

--- sanity/schemas/restaurant.js
+++ sanity/schemas/restaurant.js
@@ -35,6 +35,25 @@ export default {
         type: 'string',
         title: 'Address of the Restaurant',
       validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'rating',
+      type: 'number',
+      title: 'Enter a Rating from (1-5 Stars)',
+      validation: (Rule) => Rule.required().min(1).max(5).error('Please enter a Value between 1 and 5'),
+    },
+    {
+      name: 'type',
+      title: 'Category',
+      validation: (Rule) => Rule.required(),
+      type: 'reference',
+      to: [{type: 'category'}],
+    },
+    {
+      name: 'dishes',
+      type: 'array',
+      title: 'Dishes',
+      of: [{type: 'reference', to: [{type: 'dish'}]}],
     }
   ]
 }
