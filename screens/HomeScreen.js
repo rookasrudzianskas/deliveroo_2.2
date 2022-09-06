f80026ac99582bce4e7060d20f80fc8307aab2c2
Rokas <rookas.rudzenskas@gmail.com>
2022-09-06 09:22:55 +0300

    Rokas

---
+++ screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React, {useLayoutEffect} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const HomeScreen = () => {
+    useLayoutEffect(() => {
+
+    }, []);
+
+    return (
+        <View>
+            <Text>Hello, this is working fine!</Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
