9f598a3d2ba238a63a27beebb3cdfdc9261e5bfe
Rokas <rookas.rudzenskas@gmail.com>
2022-09-06 17:24:02 +0300

    Rokas

--- components/Categories.js
+++ components/Categories.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Text, View, StyleSheet, ScrollView} from 'react-native';
 import CategoryCard from "./CategoryCard";
 
 const Categories = () => {
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+
+    }, []);
     return (
         <ScrollView contentContainerStyle={{paddingHorizontal: 15, paddingTop: 10}} horizontal={true} showsHorizontalScrollIndicator={false}>
             <CategoryCard imgUrl="https://miro.medium.com/max/480/1*1NNflMbWbsAznQp3roz5Sg.jpeg" title={'testing'} />
--- components/FeaturedRow.js
+++ components/FeaturedRow.js
@@ -22,7 +22,7 @@ type-> {
         `, {id: id}).then((data) => {
             setRestaurants(data?.restaurants);
         });
-    }, []);
+    }, [id]);
     return (
         <View>
             <View className="mt-4 flex-row items-center justify-between px-4">
