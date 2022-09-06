fbefcb889d4d6f9aef509e2a6b5b451d23b61859
Rokas <rookas.rudzenskas@gmail.com>
2022-09-06 15:58:05 +0300

    Roaks

--- sanity/schemas/schema.js
+++ sanity/schemas/schema.js
@@ -5,10 +5,10 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 // We import object and document schemas
-import blockContent from './blockContent'
 import category from './category'
 import restaurant from './restaurant'
-import author from './author'
+import dish from "./dish";
+import featured from "./featured";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -18,8 +18,8 @@ export default createSchema({
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
     restaurant,
-    author,
     category,
-    blockContent,
+      dish,
+      featured,
   ]),
 })
