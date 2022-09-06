b3e0085b8a08c0d21504743dbb20900fa5f892da
Rokas <rookas.rudzenskas@gmail.com>
2022-09-06 10:12:05 +0300

    Rokas

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import React, {useLayoutEffect} from 'react';
-import {Text, View, StyleSheet, SafeAreaView, Image} from 'react-native';
+import {Text, View, StyleSheet, SafeAreaView, Image, TextInput} from 'react-native';
 import {useNavigation} from "@react-navigation/native";
 import { Ionicons } from '@expo/vector-icons';
 import { AntDesign } from '@expo/vector-icons';
+import { MaterialIcons } from '@expo/vector-icons';
 const HomeScreen = () => {
     const navigation = useNavigation();
 
@@ -14,7 +15,7 @@ const HomeScreen = () => {
     }, []);
 
     return (
-        <SafeAreaView>
+        <SafeAreaView className="bg-white pt-5">
             <View className="flex-row pb-3 items-center mx-4 space-x-2">
                 <Image
                     source={{uri: 'https://links.papareact.com/wru'}}
@@ -27,6 +28,15 @@ const HomeScreen = () => {
                     </Text>
                 </View>
                 <AntDesign name="user" size={35} color="#00CCBB" />
+            </View>
+
+            <View className="flex-row items-center space-x-2 pb-2 mx-4">
+                <View className="flex-row space-x-2 flex-1 bg-gray-200 p-3">
+                    <Ionicons name="search-outline" size={24} color="gray" className="text-gray-200" />
+                    <TextInput placeholder={'Restaurants and cuisines'} keyboardType={'default'}/>
+
+                </View>
+                <MaterialIcons name="settings-input-component" size={24} color="#00CCBB" />
             </View>
         </SafeAreaView>
     );
