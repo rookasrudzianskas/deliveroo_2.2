6cac218946ef670b14c7cf1c089085b0ae568813
Rokas <rookas.rudzenskas@gmail.com>
2022-09-06 17:26:57 +0300

    Rokas

--- components/Categories.js
+++ components/Categories.js
@@ -1,19 +1,23 @@
 import React, {useEffect, useState} from 'react';
 import {Text, View, StyleSheet, ScrollView} from 'react-native';
 import CategoryCard from "./CategoryCard";
+import sanityClient, {urlFor} from "../sanity";
 
 const Categories = () => {
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-
+        sanityClient.fetch(`
+            *[_type == "category"]
+        `).then((data) => {
+            setCategories(data);
+        })
     }, []);
     return (
         <ScrollView contentContainerStyle={{paddingHorizontal: 15, paddingTop: 10}} horizontal={true} showsHorizontalScrollIndicator={false}>
-            <CategoryCard imgUrl="https://miro.medium.com/max/480/1*1NNflMbWbsAznQp3roz5Sg.jpeg" title={'testing'} />
-            <CategoryCard imgUrl="https://miro.medium.com/max/480/1*1NNflMbWbsAznQp3roz5Sg.jpeg" title={'testing'} />
-            <CategoryCard imgUrl="https://miro.medium.com/max/480/1*1NNflMbWbsAznQp3roz5Sg.jpeg" title={'testing'} />
-            <CategoryCard imgUrl="https://miro.medium.com/max/480/1*1NNflMbWbsAznQp3roz5Sg.jpeg" title={'testing'} />
+            {categories.map((category) => (
+                <CategoryCard key={category._id} id={category._id} title={category.name} imgUrl={urlFor(category.image).width(200).url()} />
+            ))}
         </ScrollView>
     );
 };
