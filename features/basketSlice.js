c87f1200ebb71a57b0ed6ab4e667d894f78f1b11
Rokas <rookas.rudzenskas@gmail.com>
2022-09-07 08:52:14 +0300

    Rokas

---
+++ features/basketSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    items: [],
+}
+
+export const basketSlice = createSlice({
+    name: 'basket',
+    initialState,
+    reducers: {
+        addToBasket: (state, action) => {
+            state.value += 1
+        },
+        removeFromBasket: (state, action) => {
+            state.value -= 1
+        },
+        incrementByAmount: (state, action) => {
+            state.value += action.payload
+        },
+    },
+})
+
+// Action creators are generated for each case reducer function
+export const { addToBasket, removeFromBasket } = basketSlice.actions
+
+export default basketSlice.reducer
