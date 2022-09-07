37b19ba247d226cbe6a1121e543377831a60f801
Rokas <rookas.rudzenskas@gmail.com>
2022-09-07 08:44:20 +0300

    Rokas

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,28 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import Currency from 'react-currency-formatter';
 import {urlFor} from "../sanity";
+import { SimpleLineIcons } from '@expo/vector-icons';
 
 const DishRow = ({id, name, description, price, image}) => {
+    const [isPressed, setIsPressed] = useState(false);
     return (
-        <TouchableOpacity className="bg-white border p-4 border-gray-200">
-            <View className="flex-row">
-                <View className="flex-1 pr-2">
-                    <Text className="text-lg mb-1">{name}</Text>
-                    <Text className="text-gray-400">{description}</Text>
-                    <Text className="text-gray-400 mt-2">
-                        <Currency
-                            quantity={price}
-                            currency="GBP"
-                        />
-                    </Text>
-                </View>
+        <>
+            <TouchableOpacity onPress={() => setIsPressed(!isPressed)} className={`bg-white border p-4 border-gray-200 ${isPressed && 'border-b-0'}`}>
+                <View className="flex-row">
+                    <View className="flex-1 pr-2">
+                        <Text className="text-lg mb-1">{name}</Text>
+                        <Text className="text-gray-400">{description}</Text>
+                        <Text className="text-gray-400 mt-2">
+                            <Currency
+                                quantity={price}
+                                currency="GBP"
+                            />
+                        </Text>
+                    </View>
 
-                <View>
-                    <Image style={{borderWidth: 1, borderColor: '#F3F3F4'}} source={{uri: 'https://img.freepik.com/premium-photo/big-hamburger-with-double-beef-french-fries_252907-8.jpg?w=2000'}} className="h-20 w-20 bg-gray-300 p-4" />
+                    <View>
+                        <Image style={{borderWidth: 1, borderColor: '#F3F3F4'}} source={{uri: 'https://img.freepik.com/premium-photo/big-hamburger-with-double-beef-french-fries_252907-8.jpg?w=2000'}} className="h-20 w-20 bg-gray-300 p-4" />
+                    </View>
                 </View>
-            </View>
-        </TouchableOpacity>
+            </TouchableOpacity>
+
+            {isPressed && (
+                <View className="bg-white px-4">
+                    <View className="flex-row items-center space-x-2 pb-3">
+                        <TouchableOpacity>
+                            <SimpleLineIcons name="minus" size={20} color={'#00CCBB'}
+                                             // color={items.length > 0 ? "#00CCBB" : "gray"}
+                            />
+                        </TouchableOpacity>
+                        <Text>0</Text>
+                        <TouchableOpacity>
+                            <SimpleLineIcons name="plus" size={20} color={"#00CCBB"}
+                                // color={items.length > 0 ? "#00CCBB" : "gray"}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            )}
+        </>
     );
 };
 
