246d9db2ca19ce50958ce3fd4847be07763267da
Rokas <rookas.rudzenskas@gmail.com>
2022-09-07 08:38:01 +0300

    Rokas

--- components/DishRow.js
+++ components/DishRow.js
@@ -1,19 +1,26 @@
 import React from 'react';
-import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
+import {Text, View, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import Currency from 'react-currency-formatter';
+import {urlFor} from "../sanity";
 
 const DishRow = ({id, name, description, price, image}) => {
     return (
-        <TouchableOpacity>
-            <View>
-                <Text className="text-lg mb-1">{name}</Text>
-                <Text className="text-gray-400">{description}</Text>
-                <Text className="text-gray-400 mt-2">
-                    <Currency
-                        quantity={price}
-                        currency="GBP"
-                    />
-                </Text>
+        <TouchableOpacity className="bg-white border p-4 border-gray-200">
+            <View className="flex-row">
+                <View className="flex-1 pr-2">
+                    <Text className="text-lg mb-1">{name}</Text>
+                    <Text className="text-gray-400">{description}</Text>
+                    <Text className="text-gray-400 mt-2">
+                        <Currency
+                            quantity={price}
+                            currency="GBP"
+                        />
+                    </Text>
+                </View>
+
+                <View>
+                    <Image style={{borderWidth: 1, borderColor: '#F3F3F4'}} source={{uri: 'https://img.freepik.com/premium-photo/big-hamburger-with-double-beef-french-fries_252907-8.jpg?w=2000'}} className="h-20 w-20 bg-gray-300 p-4" />
+                </View>
             </View>
         </TouchableOpacity>
     );
